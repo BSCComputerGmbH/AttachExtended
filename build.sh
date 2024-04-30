67d778eb8837af6cdadf1253098d4e0e8b76cdda
m.olbertz <m.olbertz@bscgmbh.de>
2024-04-30 15:10:06 +0200

    Initial NFC Commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+export JAVA_HOME="/opt/graalvm-svm-java17-linux-gluon-22.1.0.1-Final"
+export GRAALVM_HOME="/opt/graalvm-svm-java17-linux-gluon-22.1.0.1-Final"
+export MAVEN_HOME="/opt/apache-maven-3.6.3"
+
+export ANDROID_NDK="/home/michael/.gluon/substrate/Android/cmdline-tools/ndk/25.2.9519653"
+export ANDROID_SDK="/home/michael/.gluon/substrate/Android/cmdline-tools"
+
+./gradlew clean publishToMavenLocal
+
