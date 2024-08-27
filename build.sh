2948d35cb8cad51d471d4ddbf166237174090252
Matthias <mg_73@gmx.de>
2024-08-27 16:33:25 +0200

    /**/

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 if [ $1 = "mg" ]
 then
 	echo $1
+	#Environment for Matthias
 	export JAVA_HOME="/home/mg/graalvm-svm-java17-linux-gluon-22.1.0.1-Final"
 	export GRAALVM_HOME="/home/mg/graalvm-svm-java17-linux-gluon-22.1.0.1-Final"
 	export MAVEN_HOME="/usr/share/maven"
@@ -11,7 +12,7 @@ then
 	export ANDROID_SDK="/home/mg/.gluon/substrate/Android/"
 	export ANDROID_NDK="/home/mg/.gluon/substrate/Android/ndk/25.2.9519653/"
 else
-	#Umgebung fuer Michael
+	#Environment for Michael
 	export JAVA_HOME="/opt/graalvm-svm-java17-linux-gluon-22.1.0.1-Final"
 	export GRAALVM_HOME="/opt/graalvm-svm-java17-linux-gluon-22.1.0.1-Final"
 	export MAVEN_HOME="/opt/apache-maven-3.6.3"
